920e3a30228b11142dfb20cde312d24c068c4920
yinchunyu <542968439@qq.com>
2023-03-24 15:21:53 +0800

    修复一些问题

--- Nuxt-web/plugins/baidu.ts
+++ Nuxt-web/plugins/baidu.ts
@@ -2,5 +2,6 @@ export default () => {
   const hm = document.createElement("script");
   hm.src = "https://hm.baidu.com/hm.js?9edadaa49ae4e9c979c6724865c04b05";
   const s = document.getElementsByTagName("script")[0];
+  // @ts-ignore
   s.parentNode.insertBefore(hm, s);
 }
