aaac5f01dd9ab43abd55165c0c3c5aa786c0497b
yinchunyu <542968439@qq.com>
2023-04-11 17:52:59 +0800

    提交一下

--- Nuxt-web/pages/user/index.vue
+++ Nuxt-web/pages/user/index.vue
@@ -1,11 +1,43 @@
 <template>
-  <div class="container">
-    用户中心
+  <div class="container user-index">
+    <Head>
+      <Title>1</Title>
+    </Head>
+    <el-row :gutter="10">
+     <el-col :span="6">
+       <div class="over-avatar">5</div>
+       <div class="user-index__personal-name">542968439@qq.com</div>
+     </el-col>
+      <el-col :span="18"></el-col>
+    </el-row>
   </div>
 </template>
 
 <script setup>
 definePageMeta({
-  middleware: ["auth"]
+  middleware: ["auth"],
+  layout: false
 })
 </script>
+
+<style lang="scss" scoped>
+.user-index {
+  .over-avatar {
+    width: 160px;
+    height: 160px;
+    background: $drak-blue;
+    border-radius: 50%;
+    color: #FFFFFF;
+    text-align: center;
+    line-height: 160px;
+    font-size: 60px;
+    margin: 0 auto;
+  }
+  &__personal-name {
+    text-align: center;
+    font-size: 16px;
+    font-weight: bold;
+    padding: 10px 0;
+  }
+}
+</style>
