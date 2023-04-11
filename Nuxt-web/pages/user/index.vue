01e22ee39694cef505f50520c04bbf47027fe52f
yinchunyu <542968439@qq.com>
2023-04-11 22:14:08 +0800

    提交更新

--- Nuxt-web/pages/user/index.vue
+++ Nuxt-web/pages/user/index.vue
@@ -2,22 +2,46 @@
   <div class="container user-index">
     <Head>
       <Title>1</Title>
+      <Style type="text/css" children="body { background-color: #f7f7f7; }" />
     </Head>
-    <el-row :gutter="10">
+    <el-row :gutter="30" class="mt-20">
      <el-col :span="6">
-       <div class="over-avatar">5</div>
-       <div class="user-index__personal-name">542968439@qq.com</div>
+       <div class="bg-fff user-index__head flex">
+         <img src="../../assets/images/toux.png" alt="">
+         <div>
+           542968439@qq.com
+           <p class="grey">ID: 449892</p>
+           <a class="lv lv1"></a>
+         </div>
+       </div>
      </el-col>
-      <el-col :span="18"></el-col>
+      <el-col :span="18" class="bg-fff">
+        <el-tabs
+            v-model="activeName"
+            class="demo-tabs"
+            @tab-click="handleClick"
+        >
+          <el-tab-pane label="我的收藏" name="first">我的收藏</el-tab-pane>
+          <el-tab-pane label="我的点赞" name="second">我的点赞</el-tab-pane>
+          <el-tab-pane label="我的评论" name="third">我的评论</el-tab-pane>
+        </el-tabs>
+      </el-col>
     </el-row>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import type { TabsPaneContext } from 'element-plus'
+
 definePageMeta({
-  middleware: ["auth"],
-  layout: false
+  middleware: ["auth"]
 })
+
+const activeName = ref('first')
+
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+  console.log(tab, event)
+}
 </script>
 
 <style lang="scss" scoped>
@@ -38,6 +62,24 @@ definePageMeta({
     font-size: 16px;
     font-weight: bold;
     padding: 10px 0;
+  }
+  &__head {
+    padding: 20px;
+    > img {
+      width: 80px;
+      margin-right: 20px;
+    }
+    .lv {
+      background: url('../../assets/images/jlt.png') no-repeat 0 0;
+      display: inline-block;
+      width: 42px;
+      height: 22px;
+      vertical-align: middle;
+      margin-top: 15px;
+      &.lv1 {
+        background-position: 0 -373px;
+      }
+    }
   }
 }
 </style>
