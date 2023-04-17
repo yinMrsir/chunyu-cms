60571e096d0e52a415276ea835402e0bc2397f17
yinchunyu <542968439@qq.com>
2023-04-17 15:17:14 +0800

    此处恢复移步至nginx处理

--- Nuxt-web/composables/useHttp.ts
+++ Nuxt-web/composables/useHttp.ts
@@ -1,19 +1,11 @@
 import {SearchParams} from "ohmyfetch";
 
-const getDomain = (url: string) => {
-    let domain: string[] = url.split("/");
-    return domain[2] || ''
-}
-
 export const useGet = <T>(url: string, params?: SearchParams, headers?: any): Promise<T> => {
     const { baseUrl } = useRuntimeConfig()
     return $fetch(baseUrl + url, {
         method: 'get',
         params,
-        headers: {
-            ...headers,
-            host: getDomain(baseUrl)
-        }
+        headers
     })
 }
 
@@ -22,10 +14,7 @@ export const usePost = <T>(url: string, body?: Record<string, any>, headers?: an
     return $fetch(baseUrl + url, {
         method: 'post',
         body,
-        headers: {
-            ...headers,
-            host: getDomain(baseUrl)
-        }
+        headers
     })
 }
 
@@ -34,9 +23,6 @@ export const useDelete = <T>(url: string, query?: SearchParams, headers?: any): 
     return $fetch(baseUrl + url, {
         method: 'delete',
         query,
-        headers: {
-            ...headers,
-            host: getDomain(baseUrl)
-        }
+        headers
     })
 }
