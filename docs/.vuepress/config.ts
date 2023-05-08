fc5c36cba06a99ea2fdabaa0979536287e293cfa
yinchunyu <542968439@qq.com>
2023-05-08 11:12:27 +0800

    添加文档目录

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,7 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: '淳渔CMS',
+  description: '淳渔CMS-Nodejs快速搭建影视，视频类网站的系统，它基于nuxt3、vue3、nestjs，也非常适合二次开发。',
+})
